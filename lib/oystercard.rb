36d273fe4e437d959552b1b46e8f31adf6f12c29
Shereen Finney <shereenfinney@gmail.com>
2017-01-10 12:48:38 +0000

    Second rspec test - to check the top_up method is invoked with one argument

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,6 +2,7 @@
 
 class Oystercard
 
+# 1st rspec test
 attr_reader :balance # # You need this to make the instance variable '@balance' below available to the instance when calling it directly. e.g. card = Oystercard.new, card.balance won't work, instead use card.get_balance but a SHORTCUT is to use attr_reader.
 
   def initialize
@@ -12,5 +13,12 @@ attr_reader :balance # # You need this to make the instance variable '@balance' 
 # def get_balance
 #   @balance
 # end
+
+# 2nd rspec test
+  def top_up(amt)
+    # amt = topup + amt
+  end
+
+
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -10,7 +10,15 @@ describe Oystercard do
     end
   end
 
-
+  context "if a top_up method is invoked with the top-up value as the arg" do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+  end
+  #
+  # context "if a top_up method is invoked with the top-up value as the arg" do
+  #    it "increases balance by top_up value" do
+  #      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+  #   end
+  # end
 
 
 
