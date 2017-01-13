b98c5a577ea9d6b42855bb99893875c1f896173c
Shereen Finney <shereenfinney@gmail.com>
2017-01-13 11:52:43 +0000

    Refactored oystercard.rb and oystercard_spec.rb

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,41 +3,27 @@
 class Oystercard
 
 BALANCE_LIMIT = 90
-FARE = 1
-
-# 1st rspec test
-attr_reader :balance #:entry_station
-
-# # You need this to make the instance variable '@balance' below available to the instance when calling it directly. e.g. card = Oystercard.new, card.balance won't work, instead use card.get_balance but a SHORTCUT is to use attr_reader.
+MIN_FARE = 1
+attr_reader :balance
 
   def initialize
     @balance = 0
-    @in_use = false #initially the card isn't in use
-    # @entry_station = " "
+    @in_use = false
   end
 
-## Using attr_reader is a SHORTCUT for this method
-# def get_balance
-#   @balance
-# end
-
-# 2nd - 4th rspec tests
   def top_up(amount)
-   raise "ERROR! You have exceeded your set balance limit of £#{BALANCE_LIMIT}" if @balance + amount > BALANCE_LIMIT
-  #@balance = @balance + top_up_amt has been refactored to:
-  @balance =+ amount
+    raise exceed_limit_msg if @balance + amount > BALANCE_LIMIT
+    @balance =+ amount
   end
 
-  # def touch_in(station)
   def touch_in
-    raise "Unable to touch in - insufficient funds" if @balance < FARE
+    raise insufficient_funds_msg if @balance < MIN_FARE
     @in_use = true
-    # @entry_station = station
   end
 
   def touch_out
     @in_use = false
-    deduct(FARE)
+    deduct(MIN_FARE)
   end
 
   def in_journey?
@@ -46,6 +32,14 @@ attr_reader :balance #:entry_station
 
   def deduct(fare)
     @balance =- fare
+  end
+
+  def exceed_limit_msg
+    "ERROR! You have exceeded your set balance limit of £#{BALANCE_LIMIT}"
+  end
+
+  def insufficient_funds_msg
+    "Unable to touch in - insufficient funds"
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,26 +3,17 @@
 require "oystercard"
 
 describe Oystercard do
-  # 1st test
   it { is_expected.to respond_to(:top_up).with(1).argument }
-
-  #6th test
   it {is_expected.to respond_to :in_journey?}
-
-  #7th test
   it {is_expected.to respond_to :touch_in}
-
-  #8th test
   it {is_expected.to respond_to :touch_out}
 
-  #2nd test
   context "When a new object is initialized" do
     it "has a default balance of 0" do
       expect(subject.balance).to eq(0)
     end
   end
 
-  #3rd test
   describe "#top_up(amount)" do
     context "when invoked with the top-up value as the arg" do
       it "increases balance by top_up value" do
@@ -30,38 +21,32 @@ describe Oystercard do
       end
     end
 
-
-    #4th test
     context "when the balance limit of £90 is exceeded" do
       it "throws an exception" do
        limit = Oystercard::BALANCE_LIMIT # you have to use the class name here, because BALANCE_LIMIT is a class constant.
         expect{ subject.top_up limit + 1 }.to raise_error("ERROR! You have exceeded your set balance limit of £#{limit}")
       end
     end
+  end
 
-  end # end of describe block
-
-
-  #9th test
   describe "#touch_in" do
     context "when the customer touches in at the barriers" do
         it "can touch in" do
-        subject.balance > Oystercard::FARE
+        subject.balance > Oystercard::MIN_FARE
         subject.top_up(10)
         subject.touch_in
         expect(subject).to be_in_journey
         end
         it "throws an error if insufficient funds" do
-        subject.balance < Oystercard::FARE
+        subject.balance < Oystercard::MIN_FARE
         expect{subject.touch_in}.to raise_error "Unable to touch in - insufficient funds"
         end
         # it "records the entry station" do
         #   expect{subject.touch_in(subject.entry_station)}.to eq (" ")
       #  end
     end
-  end # end of describe block
+  end
 
-  #10th test
   describe "#touch_out" do
     context "when the customer touches out at the barriers" do
       it "is in no longer in use" do
@@ -71,15 +56,15 @@ describe Oystercard do
           expect(subject).not_to be_in_journey
       end
     end
-    #5th test
-    describe "#deduct(fare)" do
-      context "when invoked with the fare value as the arg" do
-        it "reduces balance by the fare value" do
-          expect{ subject.deduct Oystercard::FARE }.to change{ subject.balance }.by -(Oystercard::FARE)
-        end
+  end
+
+  describe "#deduct(fare)" do
+    context "when invoked with the fare value as the arg" do
+      it "reduces balance by the fare value" do
+        expect{ subject.deduct Oystercard::MIN_FARE }.to change{ subject.balance }.by -(Oystercard::MIN_FARE)
       end
     end
-  end # end of describe block
+  end
 
 
   describe "#top_up(amount)" do
